5675fd0cf20b5f102fd0b430cf2bc8e11992f504
evansalz <31269464+evansalz@users.noreply.github.com>
2018-04-19 15:11:00 -0600

    Update StockForecaster.py

--- StockForecaster.py
+++ StockForecaster.py
@@ -1,1 +1,8 @@
+import pandas as pd
+import numpy as np
+import mlutilities as ml
 
+!wget https://www.quandl.com/api/v3/datasets/EOD/DIS.csv?api_key=q74Toz9W7ovpxJh3Vybd
+#Disney stock
+fName = 'DIS.csv?api_key=q74Toz9W7ovpxJh3Vybd'
+disney = pd.read_csv(fName, delimiter=',', engine = 'python')
