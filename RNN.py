c86b76f8b9342ba50a76e19637708c4f768a2283
rfigue <rfigue@rams.colostate.edu>
2018-05-01 11:35:30 -0600

    new external libraries and resources

--- RNN.py
+++
@@ -1,5 +0,0 @@
-#RECURRENT NEURAL NETWORKS
-
-class RNN:
-    def RNN(self):
-        print("RNN")
---
+++ TimeEmbedded.py
@@ -0,0 +1,12 @@
+#Time Embedding
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import neuralnetworksA4 as nn
+import qdalda
+import mlutilities as ml
+
+class TimeEmbedding:
+    def RNN(self):
+        print("RNN")
