9e726a3c6a2cbc7ff6b8dd049dfd4dfa2069f7da
rfigue <rfigue@rams.colostate.edu>
2018-05-03 20:32:26 -0600

    added the rollingWindow function to TimeEmbedded

--- TimeEmbedded.py
+++ TimeEmbedded.py
@@ -7,6 +7,14 @@ import neuralnetworksA4 as nn
 import qdalda
 import mlutilities as ml
 
-class TimeEmbedding:
-    def RNN(self):
-        print("RNN")
+class TimeEmbedded:
+
+    def rollingWindows(X, windowSize):
+        nSamples, nAttributes = X.shape
+        nWindows = nSamples - windowSize + 1
+        # Shape of resulting matrix
+        newShape = (nWindows, nAttributes * windowSize)
+        itemSize = X.itemsize  # number of bytes
+        # Number of bytes to increment to starting element in each dimension
+        strides = (nAttributes * itemSize, itemSize)
+        return np.lib.stride_tricks.as_strided(X, shape=newShape, strides=strides)
