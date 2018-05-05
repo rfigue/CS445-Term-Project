94d88affabc2122825d0b7136a31150a726e0114
evansalz <31269464+evansalz@users.noreply.github.com>
2018-05-05 14:08:21 -0600

    Update RNN.py

--- RNN.py
+++ RNN.py
@@ -1,6 +1,7 @@
 import time
 import numpy as np
 from numpy import newaxis
+#The code requires installation of keras, which is a dependency of this class
 from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.recurrent import LSTM
 from keras.models import Sequential
@@ -10,58 +11,59 @@ import matplotlib.pyplot as plt
 def plotResults(predicted, actual, length):
     fig = plt.figure(facecolor='white')
     fig2 = fig.add_subplot(111)
-    fig2.plot(actual, label='Actual')
+    fig2.plot(actual, label='Actual Close')
     print('T0')
 
-    for i, data in enumerate(predicted):
-        padding = [None for p in xrange(i * length)]
-        plt.plot(padding + data, label='Prediction')
+    for i, item in enumerate(predicted):
+        for p in xrange(i * length):
+            temp = [None]
+        plt.plot(item + temp, label='Predicted Close')
         plt.legend()
     plt.show()
 
 def normalize(nWindows):
-    n = []
-    for i in nWindows:
-        n = [((float(p) / float(i[0])) - 1) for p in i]
-        n.append(n)
-    return n
+    resNorm = []
+    for w in nWindows:
+        for i in w:
+            norm = [(float(i) / float(w[0])) - 1]
+        resNorm.append(norm)
+    return resNorm
 
 def model(layers):
     model = Sequential()
-    model.add(LSTM(input_dim=layers[0], output_dim=layers[1], return_sequences=True))
+    model.add(LSTM(input_dim = layers[0], output_dim = layers[1], return_sequences = True))
     model.add(Dropout(0.2))
-    model.add(LSTM(layers[2], return_sequences=False))
+    model.add(LSTM(layers[2], return_sequences = False))
     model.add(Dropout(0.2))
-
-    model.add(Dense(output_dim=layers[3]))
+    model.add(Dense(output_dim = layers[3]))
     model.add(Activation("linear"))
 
     start = time.time()
-    model.compile(loss="mse", optimizer="rmsprop")
+    model.compile(loss = "rmse", optimizer = "rmsprop")
     print("Compilation Time : ", time.time() - start)
     return model
 
-def onePointPrediction(model, data):
-    predicted = model.predict(data)
-    return np.reshape(predicted, (predicted.size,))
+def onePoint(model, data):
+    resPredictions = model.predict(data)
+    return np.reshape(resPredictions, (resPredictions.size,))
 
-def newPointPrediction(model, data, window):
-    current = data[0]
+def newPoint(model, data, window):
     predicted = []
+    current = data[0]
     for i in xrange(len(data)):
         predicted.append(model.predict(current[newaxis,:,:])[0,0])
         current = current[1:]
         current = np.insert(current, [window-1], predicted[-1], axis=0)
     return predicted
 
-def multiPointPredictions(model, data, window, length):
+def multiPoint(model, data, window, length):
     predicted = []
     for i in xrange(len(data)/length):
         current = data[i*length]
-        pred = []
+        tempPreds = []
         for item in xrange(length):
-            pred.append(model.predict(current[newaxis,:,:])[0,0])
+            tempPreds.append(model.predict(current[newaxis,:,:])[0,0])
             current = current[1:]
-            current = np.insert(current, [window-1], pred[-1], axis=0)
-        predicted.append(pred)
+            current = np.insert(current, [window-1], tempPreds[-1], axis=0)
+        predicted.append(tempPreds)
     return predicted
